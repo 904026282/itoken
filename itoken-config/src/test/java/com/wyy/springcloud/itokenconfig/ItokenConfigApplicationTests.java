cc68f8f9e39bbe5c5f31d80d148b82eebe7a1ec0
wyy <904026282@qq.com>
2019-11-11 15:37:44 +0800

    Initial commit

---
+++ itoken-config/src/test/java/com/wyy/springcloud/itokenconfig/ItokenConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wyy.springcloud.itokenconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItokenConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
